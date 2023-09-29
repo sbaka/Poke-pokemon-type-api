26ad0814d76f0cb0c0018ba0a78c7a391bc18578
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-09-29 16:21:52 +0200

    ✨ : add application shell

---
+++ src/main/java/fr/univ_lille/alom/pokemon_type_api/PokemonTypeApiApplication.java
@@ -0,0 +1,13 @@
+package fr.univlille.alom.pokemon_type_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTypeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTypeApiApplication.class, args);
+	}
+
+}
